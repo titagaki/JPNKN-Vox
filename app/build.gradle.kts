81b28b47b4f6f4951c9dd6cbe6264b220506d5e5
titagaki <megane.wo.gurun@gmail.com>
2026-03-02 00:33:03 +0900

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ android {
             )
         }
     }
+
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
